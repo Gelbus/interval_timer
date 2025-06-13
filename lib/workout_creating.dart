c86fb9bba0bd639bf0a0b824d6c5100682911d87
Gleb <GNechto@yandex.ru>
2025-06-13 12:42:28 +0300

    - Добавлено текстовое поле приемлемого вида для вводжа названия тренировки, но всё ещё требуется доработка

--- lib/workout_creating.dart
+++ lib/workout_creating.dart
@@ -5,15 +5,23 @@ import 'package:interval_timer/config.dart';
 class WorkoutCreating extends StatefulWidget {
   const WorkoutCreating({super.key});
 
+
   @override
   _WorkoutCreating createState() => _WorkoutCreating();
+
 }
 
 class _WorkoutCreating extends State<WorkoutCreating> {
-  List<bool> isSelected = [true, false]; // Состояние переключателя
   bool isExerciseSelected = true;
+  TextEditingController myController = TextEditingController();
+
 
   @override
+  void dispose() {
+    myController.dispose();
+    super.dispose();
+  }
+
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: BGColor,
@@ -22,39 +30,11 @@ class _WorkoutCreating extends State<WorkoutCreating> {
         alignment: Alignment.center,
         child: Column(
           children: [
+            _textField(),
 
-            SizedBox(height: 30,),
+            SizedBox(height: 10,),
 
-            Container(
-              decoration: BoxDecoration(
-                color: Color(0xff1B2A41),
-                borderRadius: BorderRadius.circular(30),
-              ),
-              padding: const EdgeInsets.all(4),
-              child: Row(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  _buildAnimatedButton(
-                    text: "Работа",
-                    isSelected: isExerciseSelected,
-                    onTap: () {
-                      setState(() {
-                        isExerciseSelected = true;
-                      });
-                    },
-                  ),
-                  _buildAnimatedButton(
-                    text: "Отдых",
-                    isSelected: !isExerciseSelected,
-                    onTap: () {
-                      setState(() {
-                        isExerciseSelected = false;
-                      });
-                    },
-                  ),
-                ],
-              ),
-            ),
+            _choiceSlider(),
           ],
         ),
       ),
@@ -77,6 +57,79 @@ class _WorkoutCreating extends State<WorkoutCreating> {
       elevation: 0.0,
       scrolledUnderElevation: 0.0,
       centerTitle: true,
+    );
+  }
+
+  Container _textField() {
+    return Container(
+      padding: EdgeInsets.only(left: 20, right: 20),
+      child: TextField(
+        controller: myController,
+        decoration: InputDecoration(
+          hintText: 'Введите название тренировки',
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(30),
+
+          ), // граница поля
+          suffixIcon: IconButton( // иконка после текста
+            icon: Icon(Icons.clear),
+            onPressed: () => myController.clear(),
+          ),
+          filled: true, // заливка фона
+          fillColor: ElementsColor, // цвет фона
+        ),
+        keyboardType: TextInputType.text,
+        textInputAction: TextInputAction.done, // действие кнопки "Готово"
+        style: TextStyle( // стиль текста
+            fontSize: 16,
+            color: Colors.white,
+            fontFamily: 'base'
+        ),
+        textAlign: TextAlign.start, // выравнивание текста
+        maxLength: 14, // максимальное количество символов
+        cursorColor: Colors.blue, // цвет курсора
+        cursorWidth: 2.0, // ширина курсора
+        cursorHeight: 20.0, // высота курсора
+        onChanged: (text) { // обработчик изменения текста
+          print('Текст изменился: $text');
+        },
+        onSubmitted: (text) { // обработчик нажатия "Готово"
+          print('Отправлено: $text');
+        },
+      ),
+    );
+  }
+
+  Container _choiceSlider() {
+    return Container(
+      decoration: BoxDecoration(
+        color: Color(0xff1B2A41),
+        borderRadius: BorderRadius.circular(30),
+      ),
+      padding: const EdgeInsets.all(4),
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          _buildAnimatedButton(
+            text: "Работа",
+            isSelected: isExerciseSelected,
+            onTap: () {
+              setState(() {
+                isExerciseSelected = true;
+              });
+            },
+          ),
+          _buildAnimatedButton(
+            text: "Отдых",
+            isSelected: !isExerciseSelected,
+            onTap: () {
+              setState(() {
+                isExerciseSelected = false;
+              });
+            },
+          ),
+        ],
+      ),
     );
   }
 
