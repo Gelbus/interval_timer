59a690913a5e69aee03f98758882e8e80c2c934c
Gleb <GNechto@yandex.ru>
2025-06-14 17:15:28 +0300

    - Доделана страница создания и редктирования тренировок

--- lib/workout_creating.dart
+++ lib/workout_creating.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:interval_timer/config.dart';
+import 'package:numberpicker/numberpicker.dart';
 
 
 class WorkoutCreating extends StatefulWidget {
@@ -14,6 +16,11 @@ class WorkoutCreating extends StatefulWidget {
 class _WorkoutCreating extends State<WorkoutCreating> {
   bool isExerciseSelected = true;
   TextEditingController myController = TextEditingController();
+
+  int seconds = 1;
+  int minutes = 0;
+
+
 
 
   @override
@@ -35,6 +42,18 @@ class _WorkoutCreating extends State<WorkoutCreating> {
             SizedBox(height: 10,),
 
             _choiceSlider(),
+
+            SizedBox(height: 20,),
+
+            _timeChanger(),
+
+            SizedBox(height: 20,),
+
+            _stepsList(),
+
+            SizedBox(height: 20,),
+
+            _addStep(),
           ],
         ),
       ),
@@ -52,6 +71,18 @@ class _WorkoutCreating extends State<WorkoutCreating> {
           fontWeight: FontWeight.bold
         ),
       ),
+      actions: [
+        IconButton(
+          icon: Icon(
+            Icons.add,
+            size: 35
+          ),
+          onPressed: () {
+            print('Добавить тренировку');
+            Navigator.pushNamed(context, '/');
+          },
+        ),
+      ],
       iconTheme: IconThemeData(color: Colors.white),
       backgroundColor: BGColor,
       elevation: 0.0,
@@ -158,6 +189,199 @@ class _WorkoutCreating extends State<WorkoutCreating> {
             fontFamily: 'base'
           ),
         ),
+      ),
+    );
+  }
+
+  Container _timeChanger() {
+    return  Container(
+        decoration: BoxDecoration(
+          // color: Colors.red
+        ),
+        child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              NumberPicker(
+                minValue: 0,
+                maxValue: 59,
+                value: minutes,
+                zeroPad: true,
+                infiniteLoop: true,
+
+                itemHeight: 70,
+                itemCount: 3,
+                onChanged: (value) {
+                  setState(() {
+                    minutes = value;
+                  });
+                },
+                selectedTextStyle: TextStyle(
+                    fontSize: 60,
+                    color: Colors.white,
+                    fontFamily: 'base'
+                ),
+                textStyle: TextStyle(
+                    fontSize: 40,
+                    color: Colors.grey,
+                    fontFamily: 'base'
+                ),
+              ),
+
+              Text(
+                ':',
+                style: TextStyle(
+                    fontSize: 70,
+                    color: Colors.white,
+                    fontFamily: 'base'
+                ),
+              ),
+
+              NumberPicker(
+                minValue: 1,
+                maxValue: 59,
+                value: seconds,
+                zeroPad: true,
+                infiniteLoop: true,
+                itemHeight: 70,
+                itemCount: 3,
+                onChanged: (value) {
+                  setState(() {
+                    seconds = value;
+                  });
+                },
+                selectedTextStyle: TextStyle(
+                    fontSize: 60,
+                    color: Colors.white,
+                    fontFamily: 'base'
+                ),
+                textStyle: TextStyle(
+                    fontSize: 40,
+                    color: Colors.grey,
+                    fontFamily: 'base'
+                ),
+                // decoration: BoxDecoration(
+                //   border: Border(
+                //     top: BorderSide(
+                //       color: Colors.white,
+                //       width: 1
+                //     ),
+                //     bottom: BorderSide(
+                //       color: Colors.white,
+                //       width: 1
+                //     )
+                //   ),
+                // )
+
+              ),
+            ]
+        )
+    );
+  }
+
+  Expanded  _stepsList(){
+    return Expanded(
+      child: ListView.separated(
+        padding: EdgeInsets.only(left: 20, right: 20),
+        itemCount: 101,
+        scrollDirection: Axis.vertical,
+        separatorBuilder: (context, index) => SizedBox(height: 25,),
+        itemBuilder: (context, index) {
+          return Container(
+              alignment: Alignment.center,
+              width: 300,
+              height: 50,
+              decoration: BoxDecoration(
+                  color: ElementsColor,
+                  borderRadius: BorderRadius.circular(16)
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    width: 150,
+                    height: 40,
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      '$index Работа',
+                      style: TextStyle(
+                        fontSize: 25,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+
+                  Container(
+                    width: 100,
+                    height: 40,
+                    alignment: Alignment.centerRight,
+                    child: Text(
+                      '$minutes : $seconds',
+                      style: TextStyle(
+                        fontSize: 25,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+
+                  Container(
+                    width: 80,
+                    height: 40,
+                    alignment: Alignment.centerRight,
+                    child: IconButton(
+                      onPressed: () {
+                      },
+                      icon: Icon(
+                        Icons.remove_circle_outline,
+                        color: Colors.white,
+                        size: 25,
+                      ),
+                    ),
+                  )
+                ],
+              )
+          );
+        },
+      ),
+    );
+  }
+
+  Padding _addStep() {
+    return Padding(
+      padding: EdgeInsets.only(bottom: 40),
+      child: ElevatedButton(
+          onPressed: () {
+
+          },
+          style: ElevatedButton.styleFrom(
+            backgroundColor: ElementsColor,
+            minimumSize: const Size(300, 70),
+            maximumSize: const Size(300, 70),
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SvgPicture.asset(
+                'assets/icons/plus.svg',
+                width: 30,
+                height: 30,
+                colorFilter: ColorFilter.mode(
+                    Colors.white,
+                    BlendMode.srcIn
+                ),
+              ),
+
+              SizedBox(width: 10),
+
+              const Text(
+                'Добавить шаг',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20,
+                  fontFamily: 'base'
+                ),
+              )
+            ],
+          )
       ),
     );
   }
