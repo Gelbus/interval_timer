f99e49d40b212a3495a0d080d0e3f1087986e041
Gleb <GNechto@yandex.ru>
2025-07-04 23:40:22 +0300

    - Добавлен общий вид окна тренировки, нужно добавить паузу и прописать логику

--- lib/workout.dart
+++ lib/workout.dart
@@ -18,10 +18,16 @@ class _Workout extends State<Workout> {
 
   late List<TrainingElement>? steps;
 
+  int currentStep = 0;
+  int minutes = 0;
+  int seconds = 0;
+
   @override
   void initState() {
     super.initState();
     steps = data?[widget.currentTraining].steps;
+    minutes = data![widget.currentTraining].hours * 60 + data![widget.currentTraining].minutes;
+    seconds = data![widget.currentTraining].seconds;
   }
 
   @override
@@ -33,15 +39,11 @@ class _Workout extends State<Workout> {
         alignment: Alignment.center,
         child: Column(
           children: [
-            Text(
-              "${widget.currentTraining}",
-              style: TextStyle(
-                  fontSize: 30,
-                  fontFamily: 'base',
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold
-              ),
-            ),
+            _generalTimer(),
+            SizedBox(height: 20),
+            _stepTimer(),
+            _activityType(),
+            SizedBox(height: 20),
             _stepsList(),
 
           ],
@@ -73,6 +75,45 @@ class _Workout extends State<Workout> {
       elevation: 0.0,
       scrolledUnderElevation: 0.0,
       centerTitle: true,
+    );
+  }
+
+  Text _generalTimer()
+  {
+    return Text(
+      "${minutes ~/ 10}${minutes % 10} : ${seconds ~/ 10}${seconds % 10}",
+      style: TextStyle(
+          fontSize: 30,
+          fontFamily: 'base',
+          color: Colors.white,
+          fontWeight: FontWeight.bold
+      ),
+    );
+  }
+
+  Text _stepTimer()
+  {
+    return Text(
+      "${minutes ~/ 10}${minutes % 10} : ${seconds ~/ 10}${seconds % 10}",
+      style: TextStyle(
+          fontSize: 80,
+          fontFamily: 'base',
+          color: Colors.white,
+          fontWeight: FontWeight.bold
+      ),
+    );
+  }
+
+  Text _activityType()
+  {
+    return Text(
+      "Отдых",
+      style: TextStyle(
+          fontSize: 50,
+          fontFamily: 'base',
+          color: Colors.green,
+          fontWeight: FontWeight.bold
+      ),
     );
   }
 
